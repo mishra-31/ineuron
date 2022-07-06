438851bac310da46f34bd25760a011af6669e27f
Abhishek Mishra <82857542+mishra-31@users.noreply.github.com>
2022-07-07 03:09:14 +0530

    Add files via upload

---
+++ Assignments/tuple_class/tuple_class.py
@@ -0,0 +1,30 @@
+import logging
+
+logging.basicConfig(filename='tuple.log',level=logging.DEBUG,format='%(asctime)s %(name)s %(levelname)s %(message)s')
+
+class Tuple:
+
+    def __init__(self,collection):
+        self.collection = collection
+
+    # Try to extract all the tuple entities
+    def extract_tuple_ent(self):
+
+        try:
+            logging.info('Extracting tuple entities')
+            tup_ent=[]
+            for i in self.collection:
+                if type(i) == tuple:
+                    for j in i:
+                        tup_ent.append(j)
+            logging.info(tup_ent)
+            return tup_ent
+
+        except Exception as e:
+            logging.exception(e)
+
+if __name__ == '__main__':
+    l = [[1, 2, 3, 4], (2, 3, 4, 5, 6), (3, 4, 5, 6, 7), set([23, 4, 5, 45, 4, 4, 5, 45, 45, 4, 5]),
+         {'k1': "sudh", 'k2': "ineuron", 'k3': "kumar", 3: 6, 7: 8}, ["ineuron", "data science"]]
+    obj = Tuple(l)
+    print(obj.extract_tuple_ent())
