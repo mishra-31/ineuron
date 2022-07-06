bda8a7497bc0ac1a7805efa191df06b7e2835f75
Abhishek Mishra <82857542+mishra-31@users.noreply.github.com>
2022-07-07 03:10:33 +0530

    Update tuple_class.py

--- Assignments/tuple_class/tuple_class.py
+++ Assignments/tuple_class/tuple_class.py
@@ -24,7 +24,7 @@ class Tuple:
             logging.exception(e)
 
 if __name__ == '__main__':
-    l = [[1, 2, 3, 4], (2, 3, 4, 5, 6), (3, 4, 5, 6, 7), set([23, 4, 5, 45, 4, 4, 5, 45, 45, 4, 5]),
+    l = [[1, 2, 3, 4], (2, 3, 4, 5, 6), (3, 4, 5, 6, 7,8), set([23, 4, 5, 45, 4, 4, 5, 45, 45, 4, 5]),
          {'k1': "sudh", 'k2': "ineuron", 'k3': "kumar", 3: 6, 7: 8}, ["ineuron", "data science"]]
     obj = Tuple(l)
     print(obj.extract_tuple_ent())
